29678ef2c9f5de95046c00e68ec9558bad859d2b
tasiasalex <alex.tasias22@lacetania.cat>
2026-05-26 19:15:32 +0200

    Update apps.py

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
+# Configuració bàsica de l'aplicació de blog
 class BlogConfig(AppConfig):
+    # Defineixo el tipus d'ID autoincremental que faran servir els models per defecte
     default_auto_field = 'django.db.models.BigAutoField'
+    
+    # El nom de l'app tal com està a la carpeta
     name = 'blog'
